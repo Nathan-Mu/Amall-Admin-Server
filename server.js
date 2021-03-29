7b072869cc665d6f208dcfe9698f27bc3d838724
Nathan <zhaody.mu@gmail.com>
2021-03-29 20:59:54 +1100

    connect mongodb, start app after db connection

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const routes = require('./routes/index');
+const { PORT, HOST, NAME } = require('./config/db');
+const { PORT: APP_PORT } = require('./config/app');
 
 const app = express();
 
 app.use('/', routes);
 
-app.listen(9000, () => {
-	console.log('Success: Server started');
-});
+mongoose
+	.connect(`mongodb://${HOST}:${PORT}/${NAME}`, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		console.log('### Success: Database connected');
+		app.listen(APP_PORT, () => {
+			console.log(
+				`### Success: Server started on http://localhost:${APP_PORT}`
+			);
+		});
+	})
+	.catch(error => {
+		console.log('### Error: Database connected failed', error);
+	});
