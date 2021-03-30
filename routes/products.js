c82e562e9fa6b7fb01eb5785c977a7f6d1bb1278
Nathan <zhaody.mu@gmail.com>
2021-03-30 23:37:38 +1100

    products apis (wip)

--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,118 @@
 const express = require('express');
 
+const Product = require('../models/Product');
+const response = require('../utils/responseGenerator');
+const logger = require('../utils/logger');
+
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-	res.send('products');
+const DEFAULT_PAGE_SIZE = 5;
+const DEFAULT_PAGE_NO = 1;
+
+const getOptions = ({ pageSize, pageNum, count }) => {
+	if (count < pageSize * (pageNum - 1)) {
+		return { msg: 'Page not found: pageSize * (pageNum - 1) > total' };
+	}
+	let skip = count - pageNum * pageSize;
+	let limit = pageSize * 1;
+	if (skip < 0) (limit += skip), (skip = 0);
+	const options = { skip, limit };
+	return { options };
+};
+
+/*
+ * POST /products/new
+ * body: {categoryId, name, desc, price, detail, imgs}
+ */
+router.post('/new', (req, res) => {
+	const newProduct = req.body;
+	const { name } = newProduct;
+
+	Product.findOne({ name })
+		.then(product => {
+			if (product) {
+				res.send(response.bad('Product name existed'));
+				return new Promise(() => {});
+			}
+			return Product.create(newProduct);
+		})
+		.then(product => {
+			res.send(response.good({ product }));
+		})
+		.catch(error => {
+			let where = 'Create product';
+			logger.recordException(error, where);
+			res.send(response.exception(where));
+		});
+});
+
+/*
+ * GET /products/all?pageNum=x&pageSize=x
+ * query: {pageNum, pageSize}
+ */
+router.get('/all', (req, res) => {
+	const { pageNum = DEFAULT_PAGE_SIZE, pageSize = DEFAULT_PAGE_NO } = req.query;
+
+	let total;
+
+	Product.countDocuments()
+		.then(count => {
+			const { options, msg } = getOptions({ count, pageSize, pageNum });
+			if (msg) return Promise.reject(msg);
+			total = count;
+			return Product.find({}, null, options);
+		})
+		.then(products => {
+			const list = products.reverse();
+			const data = { list, pageNum, pageSize, total };
+			res.send(response.good(data));
+		})
+		.catch(error => {
+			let where = 'Get category by page no and size';
+			logger.recordException(error, where);
+			res.send(response.exception(where));
+		});
+});
+
+router.get('/search', (req, res) => {
+	const {
+		pageNum = DEFAULT_PAGE_NO,
+		pageSize = DEFAULT_PAGE_SIZE,
+		productName,
+		productDesc,
+	} = req.query;
+
+	let total;
+	let condition = {};
+
+	if (productName) {
+		condition = { name: new RegExp(`^.*${productName}.*$`) };
+	} else if (productDesc) {
+		condition = { desc: new RegExp(`^.*${productDesc}.*$`) };
+	}
+
+	Product.countDocuments(condition)
+		.then(count => {
+			const { options, msg } = getOptions({ count, pageSize, pageNum });
+			if (msg) return Promise.reject(msg);
+			total = count;
+			return Product.find(condition, null, options);
+		})
+		.then(products => {
+			res.send(
+				response.good({
+					list: products.reverse(),
+					pageNum,
+					pageSize,
+					total,
+				})
+			);
+		})
+		.catch(error => {
+			let where = 'Get category by page no and size';
+			logger.recordException(error, where);
+			res.send(response.exception(where));
+		});
 });
 
 module.exports = router;
