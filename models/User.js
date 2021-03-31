b1fec90f35285978e459e3e3ac49f16f8f7c4845
Nathan <zhaody.mu@gmail.com>
2021-03-31 15:39:40 +1100

    refactor user model

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const md5 = require('blueimp-md5');
 
 const schema = new mongoose.Schema({
-	username: { type: String, required: true },
+	username: { type: String, required: true, unique: true },
 	password: { type: String, required: true },
 	phone: String,
 	email: String,
