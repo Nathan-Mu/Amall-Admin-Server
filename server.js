9726e039cdcc0d96d870d139e0a915c9d946463b
Nathan <zhaody.mu@gmail.com>
2021-03-31 15:40:20 +1100

    set cors settings

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const logger = require('./utils/logger');
 const routes = require('./routes/index');
 const { PORT: DB_PORT, HOST: DB_HOST, NAME: DB_NAME } = require('./config/db');
 const { PORT: APP_PORT } = require('./config/app');
@@ -8,8 +9,24 @@ const tokenValidator = require('./middleware/tokenValidator');
 
 const app = express();
 
+app.use(express.static('public'));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use((req, res, next) => {
+	res.set('Access-Control-Allow-Origin', '*');
+	res.set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+	res.set(
+		'Access-Control-Allow-Headers',
+		'content-type, authorization, accept, X-Requested-With'
+	);
+	res.set('Access-Control-Max-Age', 86400);
+	if (req.method.toLowerCase() === 'options') {
+		return res.end();
+	}
+	next();
+});
 
 app.use(tokenValidator);
 
@@ -23,9 +40,9 @@ mongoose
 	.then(() => {
 		console.log('### Success: Database connected');
 		app.listen(APP_PORT, () => {
-			console.log(
-				`### Success: Server started on http://localhost:${APP_PORT}`
-			);
+			let serverDetails = `Server started on http://localhost:${APP_PORT}`;
+			console.log(`### Success: ${serverDetails}`);
+			logger.recordServerStart(serverDetails);
 		});
 	})
 	.catch(error => {
