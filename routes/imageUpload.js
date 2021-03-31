6a57fa76ded5e5d06810918de16be858f47c574e
Nathan <zhaody.mu@gmail.com>
2021-03-31 15:39:02 +1100

    finish image upload apis

--- routes/imageUpload.js
+++ routes/imageUpload.js
@@ -1,9 +1,60 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const multer = require('multer');
+
+const response = require('../utils/responseGenerator');
+const logger = require('../utils/logger');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+const dirPath = path.join(__dirname, '..', 'public/upload');
+const storage = multer.diskStorage({
+	destination: dirPath,
+	filename: (req, file, cb) => {
+		let ext = path.extname(file.originalname);
+		cb(null, `${file.fieldname}-${Date.now()}${ext}`);
+	},
+});
+const upload = multer({ storage });
+
+/*
+ * POST /img/upload
+ */
+router.post('/img/upload', (req, res) => {
 	res.send('image upload');
+	upload.single('image')(req, res, error => {
+		if (error) {
+			let where = 'Upload image';
+			logger.recordException(error, where);
+			return res.send(where);
+		} else {
+			const file = req.file;
+			res.send(
+				response.good({
+					name: file.filename,
+					url: 'http://localhost:4000/upload/' + file.filename,
+				})
+			);
+		}
+	});
+});
+
+/*
+ * POST /img/delete
+ * body: {name}
+ */
+router.post('/img/delete', (req, res) => {
+	const { name } = req.body;
+	fs.unlink(path.join(dirPath, name), error => {
+		if (error) {
+			let where = 'Delete image';
+			logger.recordException(error, where);
+			res.send(response.exception(where));
+		} else {
+			res.send(response.good());
+		}
+	});
 });
 
 module.exports = router;
